051f5a3047d2e4ec0995c1aa0bf20b1c38110769
AndreAleman <92960547+AndreAleman@users.noreply.github.com>
2025-06-23 18:04:15 -0400

    Delete lib/api_key.dart

--- lib/api_key.dart
+++
@@ -1,7 +0,0 @@
-// lib/api_key.dart
-const String googleTranslateApiKey = 'AIzaSyBJVaqqEcfutjfnThGL7W4FAdA7UFzY-5s';
-const String wooConsumerKey = 'ck_eb2568180b9e78d77b94c155ea1c9c413662aa6f';
-const String wooConsumerSecret = 'cs_d2012da7f8c3419f86c5b703a22f74aa62a3ccbf';
-
-
-//https://sanitube.us/wp-json/wc/v3/products?consumer_key=ck_eb2568180b9e78d77b94c155ea1c9c413662aa6f&consumer_secret=cs_d2012da7f8c3419f86c5b703a22f74aa62a3ccbf
